5ad0d8625aef1c6bf40ac71590be8441412bb1a6
Anton <antonsokolskyi@gmail.com>
2016-04-18 16:32:19 +0300

    add json_api route scope, add puma server

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ gem 'sqlite3'
 # support for Cross-Origin Resource Sharing (CORS) for Rack compatible web applications
 gem 'rack-cors', require: 'rack/cors'
 gem 'devise'
+gem 'puma'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'sessions', registrations: 'registrations', confirmations: 'confirmations'}
+  scope '/json_api' do
+    devise_for :users, controllers: { sessions: 'sessions', registrations: 'registrations', confirmations: 'confirmations'}
+  end
 end
